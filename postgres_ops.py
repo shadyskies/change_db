9e9567c4ff62d4465b4ae7e4eb837cc3dc316075
shadyskies <mehta.naman328@gmail.com>
2021-05-23 13:21:25 +0530

    create table postgres_ops.py + gitignore update

--- postgres_ops.py
+++ postgres_ops.py
@@ -1,18 +1,58 @@
 # for outputting to csv -->
 # for server side: COPY (SELECT * FROM foo) TO <location> WITH CSV DELIMITER ',' HEADER;
+# psycopg2 need to be installed for sql alchemy to work
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
+import os
+import pandas as pd
 
 
-def pg_dump():
-    db = create_engine('postgresql://postgres:@localhost/practice')
-    db.execute("COPY emp to '/tmp/practice2.csv' DELIMITER ',' CSV HEADER;")
+
+def connect_pg():
+    # load_dotenv()
+
+    db_connection_url = f"postgresql+psycopg2://postgres:postgres_pwd@127.0.0.1/practice"
+    db_connection = create_engine(db_connection_url)
+    return db_connection
 
 
-def get_intopostgres():
-    db = create_engine('postgresql://postgres:@localhost/practice')
+# dumps to csv in same dir
+def pg_dump(db_connection):
+    df = pd.read_sql("SELECT * FROM emp;", con=db_connection)
+    print(df.columns.ravel)
+    df.to_csv('frompostgres.csv', index=False)
 
-    db.execute("CREATE TABLE IF NOT EXISTS temp (name text, yearsofexp int, age int, salary int)")
-    db.execute("INSERT INTO temp (name, yearsofexp, age, salary) VALUES ('Doctor Strange', 1, 1, 1)")
+
+# load csv to pg
+def pg_load(db_connection, file):
+    # db_connection = db_connection.raw_connection()
+    cur = db_connection.connect()
+    df = pd.read_csv(file)
+    heads = list(df.head(1))
+    vals = df.iloc[1:2, :].values.tolist()
+    vals = vals[0]
+
+    print(heads)
+    print(vals)
+    # print(vals[0])
+
+    # formulate table creation string
+    dic = {"str": "VARCHAR[50]", "float": "FLOAT", "int": "INT"}
+    tmp = ""
+    for i in range(len(vals)):
+        tmp1 = vals[i].__class__.__name__
+        print(tmp1)
+        if i!=len(vals)-1:
+            if tmp1 in dic:
+                tmp += heads[i] + " " + dic[tmp1] + ", "
+        else:
+            tmp += heads[i] + " " + dic[tmp1]
+    print(tmp)
+    create_table = f"CREATE TABLE IF NOT EXISTS temp({tmp});"
+    cur.execute(create_table)
+    cur.commit()
+    cur.close()
 
 
-get_intopostgres()
+conn = connect_pg()
+pg_load(conn, "frommysql.csv")
